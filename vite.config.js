288eca6b0476ee8b15ffa4b3b8f38bf261ea34ac
Sara Joy <88535714+sarajw@users.noreply.github.com>
2024-08-07 10:16:03 +0200

    Update vite.config.js dark mode splash background

--- vite.config.js
+++ vite.config.js
@@ -81,7 +81,7 @@ export default defineConfig({
         description: 'Minimalistic opinionated Mastodon web client',
         // https://github.com/cheeaun/phanpy/issues/231
         // theme_color: '#ffffff',
-        background_color: "#888",
+        background_color: "#242526",
         icons: [
           {
             src: 'logo-192.png',
